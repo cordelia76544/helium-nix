197f563143132f9ce935ff26a4cb2603e51a63db
davyjones <cordeliahoward@outlook.com>
2026-05-06 14:23:37 +0800

    change flake.nix

--- flake.nix
+++ flake.nix
@@ -1,98 +1,83 @@
 {
-  description = "Helium Browser packaged for NixOS";
+  description = "Helium Browser packaged for NixOS, x86_64-linux only";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
   };
 
   outputs = {
     self,
     nixpkgs,
   }: let
-    systems = [
-      "x86_64-linux"
-      "aarch64-linux"
-    ];
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
 
-    forAllSystems = f:
-      nixpkgs.lib.genAttrs systems
-      (system: f nixpkgs.legacyPackages.${system});
+    pname = "helium";
+    version = "0.11.7.1";
+
+    src = pkgs.fetchurl {
+      url = "https://github.com/imputnet/helium-linux/releases/download/${version}/helium-${version}-x86_64.AppImage";
+      hash = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=";
+    };
+
+    appimageContents = pkgs.appimageTools.extractType2 {
+      inherit pname version src;
+    };
+
+    helium = pkgs.appimageTools.wrapType2 {
+      inherit pname version src;
+
+      extraPkgs = pkgs:
+        with pkgs; [
+          libva
+          libva-utils
+          vulkan-loader
+          mesa
+        ];
+
+      extraInstallCommands = ''
+        desktopFile="$(find ${appimageContents} -name '*.desktop' | head -n1)"
+
+        if [ -n "$desktopFile" ]; then
+          install -Dm444 "$desktopFile" "$out/share/applications/helium.desktop"
+
+          substituteInPlace "$out/share/applications/helium.desktop" \
+            --replace-regexp '^Exec=.*' 'Exec=helium %U' \
+            --replace-regexp '^Name=.*' 'Name=Helium'
+        fi
+
+        if [ -d ${appimageContents}/usr/share/icons ]; then
+          mkdir -p "$out/share/icons"
+          cp -r ${appimageContents}/usr/share/icons/* "$out/share/icons/" || true
+        fi
+
+        if [ -d ${appimageContents}/usr/share/pixmaps ]; then
+          mkdir -p "$out/share/pixmaps"
+          cp -r ${appimageContents}/usr/share/pixmaps/* "$out/share/pixmaps/" || true
+        fi
+      '';
+
+      meta = {
+        description = "Helium Browser for Linux";
+        homepage = "https://github.com/imputnet/helium-linux";
+        license = pkgs.lib.licenses.gpl3Only;
+        platforms = ["x86_64-linux"];
+        mainProgram = "helium";
+      };
+    };
   in {
-    packages = forAllSystems (pkgs: let
-      pname = "helium";
-      version = "0.12.1.1";
+    packages.${system}.default = helium;
+    packages.${system}.helium = helium;
 
-      src =
-        if pkgs.stdenv.hostPlatform.system == "x86_64-linux"
-        then
-          pkgs.fetchurl {
-            url = "https://github.com/imputnet/helium-linux/releases/download/${version}/helium-${version}-x86_64.AppImage";
-            hash = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=";
-          }
-        else if pkgs.stdenv.hostPlatform.system == "aarch64-linux"
-        then
-          pkgs.fetchurl {
-            url = "https://github.com/imputnet/helium-linux/releases/download/${version}/helium-${version}-aarch64.AppImage";
-            hash = "sha256-BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB=";
-          }
-        else throw "Unsupported system";
+    apps.${system}.default = {
+      type = "app";
+      program = "${helium}/bin/helium";
+    };
 
-      appimageContents = pkgs.appimageTools.extractType2 {
-        inherit pname version src;
-      };
-
-      helium = pkgs.appimageTools.wrapType2 {
-        inherit pname version src;
-
-        extraPkgs = pkgs:
-          with pkgs; [
-            libva
-            libva-utils
-            vulkan-loader
-            mesa
-          ];
-
-        extraInstallCommands = ''
-          desktopFile="$(find ${appimageContents} -name '*.desktop' | head -n1)"
-
-          if [ -n "$desktopFile" ]; then
-            install -Dm444 "$desktopFile" "$out/share/applications/helium.desktop"
-
-            substituteInPlace "$out/share/applications/helium.desktop" \
-              --replace-regexp '^Exec=.*' 'Exec=helium %U' \
-              --replace-regexp '^Name=.*' 'Name=Helium'
-          fi
-
-          if [ -d ${appimageContents}/usr/share/icons ]; then
-            mkdir -p "$out/share/icons"
-            cp -r ${appimageContents}/usr/share/icons/* "$out/share/icons/" || true
-          fi
-
-          if [ -d ${appimageContents}/usr/share/pixmaps ]; then
-            mkdir -p "$out/share/pixmaps"
-            cp -r ${appimageContents}/usr/share/pixmaps/* "$out/share/pixmaps/" || true
-          fi
-        '';
-
-        meta = {
-          description = "Helium Browser for Linux";
-          homepage = "https://github.com/imputnet/helium-linux";
-          license = pkgs.lib.licenses.gpl3Only;
-          platforms = systems;
-          mainProgram = "helium";
-        };
-      };
-    in {
-      default = helium;
-      helium = helium;
-    });
-
-    apps = forAllSystems (pkgs: {
-      default = {
-        type = "app";
-        program = "${self.packages.${pkgs.system}.default}/bin/helium";
-      };
-    });
+    apps.${system}.helium = {
+      type = "app";
+      program = "${helium}/bin/helium";
+    };
 
     overlays.default = final: prev: {
       helium = self.packages.${final.system}.default;
